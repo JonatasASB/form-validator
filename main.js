93925299d3d93982c4a8ae6626eb7f08f3b266a1
jonatas <jonatasabnersb@gmail.com>
2025-03-14 14:50:06 -0300

    finishing

--- main.js
+++ main.js
@@ -34,7 +34,16 @@ let FORMvalidator = {
                         }
                         break;
                     case 'min':
-
+                        if (input.value.length < ruleDetails[1])
+                            return `Minimo de ${ruleDetails[1]} caracteres`
+                        break;
+                    case 'email':
+                        if (input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if (!regex.test(input.value)) {
+                                return 'Paradrão de E-mail inválido'
+                            }
+                        }
                         break;
                 }
             }
