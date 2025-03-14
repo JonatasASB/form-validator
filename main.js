39b174a4fe4eb07250de93523416c561ed424bfc
jonatas <jonatasabnersb@gmail.com>
2025-03-14 18:21:55 -0300

    correcting error

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ let FORMvalidator = {
                         if (input.value != '') {
                             let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                             if (!regex.test(input.value)) {
-                                return 'Paradrão de E-mail inválido'
+                                return 'Padrão de E-mail inválido'
                             }
                         }
                         break;
